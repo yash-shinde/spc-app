3a856f4946dcb3c6d7972177d6a09f1b1475dc5e
Administrator <Administrator@newmedico66-pc.excelsoft.com>
2023-02-14 17:09:32 +0530

    init commit......

---
+++ petClinicSpringBoot/src/main/java/com/spc/petClinicSpringBoot/PetClinicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spc.petClinicSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicSpringBootApplication.class, args);
+	}
+
+}
---
+++ petClinicSpringBoot/src/test/java/com/spc/petClinicSpringBoot/PetClinicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spc.petClinicSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
